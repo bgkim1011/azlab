58ae92323e3238d2db8e553045af7fdcada13b49
p121740 <bgkim@sk.com>
2023-05-16 13:28:03 +0900

    update rg-2

--- modules/rg/01.vars.tf
+++ modules/rg/01.vars.tf
@@ -8,10 +8,10 @@ variable "rg_info" {
     description = "information of resource group"
     type = set(string)
     default = []
-
+}
 variable "rg_location" {
     description = "location of resource group"
     type = string
     default = {}
-}
+
 }
